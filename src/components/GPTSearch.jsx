50e25fe863ffb95fbb817a8cb9e382bf3f78939a
amanuniyal01 <amanuniyal315@gmail.com>
2025-12-26 08:50:11 +0530

    styled my GPTSearchBar and Background

--- src/components/GPTSearch.jsx
+++ src/components/GPTSearch.jsx
@@ -4,12 +4,16 @@ import GPTMovieSuggestion from './GPTMovieSuggestion'
 
 function GPTSearch() {
   return (
-    <div className=''>
+    <div className="relative gpt-bg w-full h-screen">
+      
+     
       <img
-          src="https://assets.nflxext.com/ffe/siteui/vlv3/30c8b9f4-3db9-4b3b-a1ee-8fa56531b712/web/IN-en-20251201-TRIFECTA-perspective_c7623e8e-c406-43d2-9d9a-0140ce19ac84_medium.jpg"
-          alt="Netflix Hero"
-          className="w-full h-full object-cover"
-        />
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/30c8b9f4-3db9-4b3b-a1ee-8fa56531b712/web/IN-en-20251201-TRIFECTA-perspective_c7623e8e-c406-43d2-9d9a-0140ce19ac84_medium.jpg"
+        alt="Gpt-Background"
+        className="absolute inset-0 w-full h-full object-cover -z-10"
+      />
+
+  
       <GPTSearchBar />
       <GPTMovieSuggestion />
 
--- src/components/GPTSearchBar.jsx
+++ src/components/GPTSearchBar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function GPTSearchBar() {
   return (
-    <div className="pt-40  flex justify-center px-4">
-      <form className="bg-black w-full max-w-3xl rounded-lg p-4 flex flex-col sm:flex-row gap-2">
+    <div className="z-30 pt-40  flex justify-center px-4">
+      <form className="bg-white/10  backdrop-blur-md  w-full max-w-3xl border-white/20 rounded-lg p-4 flex flex-col sm:flex-row gap-2">
         
         <input
           className="flex-grow font-bold bg-white p-4 rounded-lg"
